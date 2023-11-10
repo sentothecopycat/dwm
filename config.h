34510492659faf6d0ab5d660b88e69821add9f28
sentothecopycat <sotentothecopycat@gmail.com>
2023-11-10 18:23:59 +0100

    added config.h config.def.h

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "SymbolsNerdFont:weight=bold:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "monospace:size=15", "SymbolsNerdFont:weight=bold:size=15" };
+static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#19031d";
 static const char col_gray2[]       = "#5a0926";
 static const char col_gray3[]       = "#5a1b53";
